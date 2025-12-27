02e1c5ddeda8504981ee5034c46226dd0f81b45c
Daniel <danielmrezende@yahoo.com.br>
2025-12-27 12:16:47 -0300

    FEAT: Animação de pegar chave

--- src/modules/key/scripts/key.gd
+++ src/modules/key/scripts/key.gd
@@ -6,9 +6,11 @@ extends Area2D
 func _on_body_entered(body) -> void:
 	if body.name == "Player":
 		body.have_key = true
-		visible = false
+		#visible = false
 		audio.play()
+		$AnimationPlayer.play("pegar")
 		$CollisionShape2D.set_deferred("disabled", true)
 		await audio.finished
-		queue_free()
+		if($AnimationPlayer.animation_finished):
+			queue_free()
 		
